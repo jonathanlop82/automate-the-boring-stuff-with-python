4e15a54987c92675edbc3229f52a0e1f737f245c
jonathanlop82 <jonathanlop82@gmail.com>
2022-05-11 17:10:40 -0500

    Update chapter 7 regular expresions

--- chapter-7/regular-expresions.py
+++ chapter-7/regular-expresions.py
@@ -23,7 +23,7 @@ print(isPhoneNumber('415-555-4242'))
 print('Is Moshi moshi a phone number?')
 print(isPhoneNumber('Moshi moshi'))
 
-message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+message = 'Call me at (415)-555-1011 tomorrow. 415-555-9999 is my office.'
 for i in range(len(message)):
     chunk = message[i:i+12]
     if isPhoneNumber(chunk):
@@ -32,9 +32,12 @@ print('Done')
 
 ### Using Regex
 # Create a regex object
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumRegex = re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')
 # Pass the string you want to search, in to the regex object search() method
 mo = phoneNumRegex.search(message)
 # Call the match object group() method to return a string
 print('Phone number found: ' + mo.group() )
-
+print('group 1: ' + mo.group(1) )
+print('group 2: ' + mo.group(2) )
+print('group 0: ' + mo.group(0) )
+print(mo.groups() )
